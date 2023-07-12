02e39ed234bc4df8400572634b93d4ea8ea5d6af
mrcxmrj <marcin.zurawel@gmail.com>
2023-07-12 18:33:53 +0200

    add styling for mobile

--- src/components/SectionItem.vue
+++ src/components/SectionItem.vue
@@ -36,8 +36,19 @@ const handleClick = () => {
 </script>
 
 <template>
-    <div class="item-container">
-        <h3 :style="{ color: blockSection ? 'gray' : '' }">{{ item.name }}</h3>
+    <div
+        class="item-container"
+        :class="{ clicked: isClicked, blocked: blockSection }"
+        @click="handleClick"
+    >
+        <div class="text-container">
+            <h3 :style="{ color: blockSection ? 'gray' : '' }">
+                {{ item.name }}
+            </h3>
+            <div class="description">
+                {{ item.description }}
+            </div>
+        </div>
         <div
             class="image-container"
             :class="{ clicked: isClicked, blocked: blockSection }"
@@ -47,61 +58,86 @@ const handleClick = () => {
                 :alt="item.name"
                 width="100"
                 height="100"
-                @click="handleClick"
             />
-        </div>
-        <div class="description">
-            {{ item.description }}
         </div>
     </div>
 </template>
 
 <style>
-.item-container {
-    border-radius: 0.5em;
-    margin: 1em;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-    position: relative;
-}
-
 .image-container {
     position: relative;
-    width: 100px;
+    min-width: 100px;
     height: 100px;
     border-radius: 50%;
     overflow: hidden;
     box-sizing: border-box;
 }
 
-.image-container:hover:not(.blocked) {
+.item-container:hover:not(.blocked) .image-container {
     border: 3px solid lightgreen;
     cursor: pointer;
-}
-.clicked {
-    border: 3px solid lightgreen;
-    cursor: default !important;
-}
-
-.description {
-    display: none;
-    position: absolute;
-    z-index: 1;
-    top: calc(100% + 10px);
-    left: 50%;
-    transform: translateX(-50%);
-    padding: 0.5em;
-    background-color: black;
-    border-radius: 0.5em;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
-    text-align: center;
-    white-space: pre-line;
 }
 
 .item-container:hover .description {
     display: block;
+}
+
+@media (min-width: 768px) {
+    .item-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        position: relative;
+    }
+    .description {
+        display: none;
+        position: absolute;
+        z-index: 1;
+        top: calc(100% + 10px);
+        left: 50%;
+        transform: translateX(-50%);
+        padding: 0.5em;
+        background-color: black;
+        border-radius: 0.5em;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+        text-align: center;
+        white-space: pre-line;
+    }
+    .clicked.image-container {
+        border: 3px solid lightgreen;
+        cursor: default !important;
+    }
+}
+
+@media (max-width: 768px) {
+    .item-container {
+        background-color: white;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+        border-radius: 0.5em;
+        padding: 10px;
+        display: flex;
+        flex-direction: row-reverse;
+        justify-content: left;
+    }
+    .text-container {
+        margin-left: 10px;
+    }
+
+    h3 {
+        color: black;
+    }
+
+    .description {
+        margin-top: 0.5em;
+        color: gray;
+        display: block;
+    }
+
+    .clicked.item-container {
+        background-color: lightgreen;
+        cursor: default !important;
+    }
 }
 </style>
