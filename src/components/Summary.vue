7e37cd24ac5bf1a818fdfcac9569d3713b0994b2
mrcxmrj <marcin.zurawel@gmail.com>
2023-07-12 16:17:45 +0200

    add scope summary

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 import { endingOperations, summary } from "@/data/dataWrapper";
-import type { Values } from "@/types/types";
+import type { Scope, Values } from "@/types/types";
 import { defineComponent, computed, watch, toRef } from "vue";
 
 const props = defineProps<{
     incompleteSections: { name: string; message: string }[];
+    activeScopes: Scope[];
     values: Values;
 }>();
 const values = props.values;
 const performEndingOperations = () => {
-    console.log("calculating...");
     for (const operation of endingOperations) {
         switch (operation.type) {
             case "addValues":
@@ -34,16 +34,6 @@ const reactiveIncompleteSections = toRef(props, "incompleteSections");
 watch(reactiveIncompleteSections, (currIncompleteSections) => {
     if (currIncompleteSections.length === 0) performEndingOperations();
 });
-
-// const totalPrice = computed(() => 100); // Replace with your logic to calculate the total price
-const hasScope = computed(() => true); // Replace with your logic to determine if options are selected
-
-const formatItemText = (text: string) => {
-    return text.replace(
-        "#{multiplied_building_days,numberOfWorkingHours}#",
-        "5 working hours"
-    );
-};
 </script>
 
 <template>
@@ -52,15 +42,15 @@ const formatItemText = (text: string) => {
             {{ summary.totalPriceDescription }}{{ values.price
             }}{{ summary.currency }}
         </p>
+        <p v-if="props.activeScopes.length === 0">
+            {{ summary.noScopeDisclaimer }}
+        </p>
+        <p v-else>
+            {{ activeScopes.map((item) => item.name).join(" + ") }}
+        </p>
         <ul>
             <li v-for="section in props.incompleteSections" :key="section.name">
                 {{ section.message }}
-            </li>
-        </ul>
-        <p v-if="!hasScope">{{ summary.noScopeDisclaimer }}</p>
-        <ul v-else>
-            <li v-for="item in summary.items" :key="item.text">
-                {{ formatItemText(item.text || "") }}
             </li>
         </ul>
     </div>
